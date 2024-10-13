770a558354abc490d2f2a1f3103ac40cd17ae936
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2024-10-13 22:00:31 +0200

    Mod

--- BBCSourceConverter/src/Main.java
+++ BBCSourceConverter/src/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         new BBCSourceConverter(true).convert(
-            new File("BBC-source/elite-source.asm"),
+            new File("BBC-source/elite-source-mod.asm"),
             new File("src/elite.a99")
         );
     }
