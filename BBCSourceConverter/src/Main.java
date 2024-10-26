aa626031cfe640c0ba61858f5f832f9461e7ceab
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2024-10-26 15:56:45 +0200

    Move source

--- BBCSourceConverter/src/Main.java
+++ BBCSourceConverter/src/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         new BBCSourceConverter().convert(
-            new File("BBC-source/elite-source-mod.asm"),
+            new File("BBC-source-modified/elite-source.asm"),
             new File("src/elite.a99")
         );
     }
