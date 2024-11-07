412226d1d21198f60dfe69e0f3f2694301d89932
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2024-11-07 20:59:03 +0100

    Fixes

--- GenerateJSONFile/src/Main.java
+++ GenerateJSONFile/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         JSONGenerator generator = new JSONGenerator("bin/elite", "elite.json");
         try {
-            generator.generate(0xFF00);
+            generator.generate(0xFE00);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.exit(1);
